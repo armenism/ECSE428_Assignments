8ddcc75f383a7142c4ca2134eda13203023a6f6e
Armen Stepanians <armen.stepanians@gmail.com>
2018-03-08 01:27:09 -0500

    validInputTest1_Regular fail

--- Assignment2/TestDrivenDevelopment/src/main/java/ca/mcgill/ecse428/assignment2/PostalRateCalculator.java
+++ Assignment2/TestDrivenDevelopment/src/main/java/ca/mcgill/ecse428/assignment2/PostalRateCalculator.java
@@ -2,4 +2,12 @@ package ca.mcgill.ecse428.assignment2;
 
 public class PostalRateCalculator {
 
+	public static void main(String[] args) {
+
+	}
+
+	public static double calculateParcelRate(String source, String destination, double length, double width,
+			double height, double weight, String postType) {
+		return 0;
+	}
 }
--- Assignment2/TestDrivenDevelopment/src/test/java/ca/mcgill/ecse428/assignment2/TestPostalRateCalculator.java
+++ Assignment2/TestDrivenDevelopment/src/test/java/ca/mcgill/ecse428/assignment2/TestPostalRateCalculator.java
@@ -6,9 +6,18 @@ import org.junit.Test;
 
 public class TestPostalRateCalculator {
 
+	private static final String regular = "REGULAR";
+	private static final String xpress = "XPRESS";
+	private static final String priority = "PRIORITY";
+
+	private static final String source = "H3G2X1";
+	private static final String destination1 = "K1M0V7";
+	
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void validInputTest1_Regular() {
+		
+		// from the lookup table
+		assertEquals(10.73, PostalRateCalculator.calculateParcelRate(source, destination1, 0.1, 0.07, 0.01, 0.15, regular), 0);
 	}
 
 }
